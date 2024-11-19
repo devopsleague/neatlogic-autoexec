ea8f8b0b8193d5e52ae45e4ce80cad25535092d1
lvzk <897706680@qq.com>
2024-11-19 12:12:43 +0800

    [功能]优化自动化node表索引

--- src/main/resources/neatlogic/resources/autoexec/changelog/2024-11-17/neatlogic_tenant.sql
+++ src/main/resources/neatlogic/resources/autoexec/changelog/2024-11-17/neatlogic_tenant.sql
@@ -1,1 +1,11 @@
 ALTER TABLE `autoexec_job_phase_node_runner` DROP PRIMARY KEY,ADD PRIMARY KEY (`job_phase_id`, `node_id`) USING BTREE;
+
+ALTER TABLE `autoexec_job_phase_node_runner` DROP INDEX `idx_phaseId`,ADD INDEX `idx_nodeId`(`node_id`) USING BTREE;
+
+ALTER TABLE `autoexec_job_phase_node` DROP INDEX `uni_id`;
+
+ALTER TABLE `autoexec_job_phase_node` DROP INDEX `idx_host_port`;
+
+ALTER TABLE `autoexec_job_phase_node` DROP INDEX `idx_lcd`;
+
+ALTER TABLE `autoexec_job_phase_node` DROP INDEX `idx_resource_id_job_phase_id`;
